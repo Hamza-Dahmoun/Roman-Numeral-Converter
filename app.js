5e5bccd552cd9b07563bc00ed2d9dc97fda2cedb
Hamza <hamza.dahmoun7@gmail.com>
2019-12-19 10:35:03 +0100

    add comment as problem solving guide

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ Convert the given number into a roman numeral.
 All roman numerals answers should be provided in upper-case.
 */
 
+/*
+Examples:
+1: I
+3: III,
+4: IV, (5-1)
+5: V,
+8: VIII,(5+3)
+9: IX,(10-1)
+10: X,
+30: XXX,(10*3)
+40: XL,(50-10)
+50: L,
+80: LXXX,(50+30)
+90: XC,(100-10)
+100: C,
+300: CCC,(100*3)
+400: CD,(500-100)
+500: D,
+800: DCCC,(500+300)
+900: CM,(1000-100)
+1000: M
+*/
+
 function convertToRoman(num) {
     return num;
-   }
-   
-   convertToRoman(36);
+}
+
+convertToRoman(36);
