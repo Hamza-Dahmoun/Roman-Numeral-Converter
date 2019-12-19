9bfc5a435b3e8a12b1704b1e87b2015f11f619e4
Hamza <hamza.dahmoun7@gmail.com>
2019-12-19 11:32:17 +0100

    converted number to array of digits and logged it

--- app.js
+++ app.js
@@ -27,9 +27,35 @@ Examples:
 900: CM,(1000-100)
 1000: M
 */
+//'number' object contains numbers and their roman corresondings,
+//note that property in a js object doesnt accept a number as property name so I
+//so I added an underscore in the begining
+var number = {
+    _1: "I",
+    _5: "V",
+    _10: "X",
+    _50: "L",
+    _100: "C",
+    _500: "D",
+    _1000: "M"
+};
+function convertToRoman() {    
+    //How?
+    //1- get number & convert it to array of digits
+    //2- delete any '0' digits in the begining of the array, bcuz it has no value
+    //3- convert the digits in the array to roman starting from the right side, and using 'number' object
+    //4- Array.join("") the converted array, and return the resulted string
 
-function convertToRoman(num) {
+
+    //1- 
+    let num = document.getElementById("textInput").value;
+    let arr = num.split("");
+    
+
+
+    let str="";
+    document.getElementById("result").innerText = str;
     return num;
 }
 
-convertToRoman(36);
+//convertToRoman(36);
